622113611005d0c84fa9839130be670ee226642c
Jack Calcinius <158483483+calcinius@users.noreply.github.com>
2024-06-08 20:21:57 +0000

    added python support.

--- index.js
+++ index.js
@@ -1,52 +1,30 @@
+const { exec } = require('child_process');
 const express = require('express');
-const axios = require('axios');
-const { createCanvas, loadImage } = require('canvas');
-
 const app = express();
 const port = process.env.PORT || 3000;
-
 app.get('/', (req, res) => {
-    console.log("Service started.")
-    res.send("")
+    res.send('GitHub Profile Customization Extension');
 });
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
 
-app.get('/image/:username', async (req, res) => {
-    const { username } = req.params;
+app.get('/run-python', (req, res) => {
+    exec(`python main.py`, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error: ${error.message}`);
+            return res.status(500).send(`Error: ${error.message}`);
+        }
+        if (stderr) {
+            console.error(`stderr: ${stderr}`);
+            return res.status(500).send(`stderr: ${stderr}`);
+        }
+        console.log(`stdout: ${stdout}`);
+        res.send(`Python script output: ${stdout}`);
+    });
+});
 
-    try {
-        const response = await axios.get(`https://api.github.com/users/${username}`);
-        const data = response.data;
-
-        const width = 800;
-        const height = 200;
-        const canvas = createCanvas(width, height);
-        const context = canvas.getContext('2d');
-
-        // Draw background
-        context.fillStyle = '#e0635a';
-        context.fillRect(0, 0, width, height);
-
-        // Draw username
-        context.fillStyle = '#000000';
-        context.font = 'bold 30px serif';
-        context.fillText(data.login, 150, 70);
-
-        // Draw bio
-        context.font = '20px serif';
-        context.fillText(data.bio || 'No bio available', 150, 100);
-
-        // Draw avatar
-        const avatar = await loadImage(data.avatar_url);
-        context.drawImage(avatar, 20, 20, 100, 100);
-
-        // Set response type and send image
-        res.setHeader('Content-Type', 'image/png');
-        canvas.pngStream().pipe(res);
-    } catch (error) {
-        res.status(500).json({ error: 'Error generating image' });
-    }
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
 });
---
+++ main.py
@@ -0,0 +1,4 @@
+print("Hello world from Vercel deployed API.")
+
+for i in range(5):
+    print(f"Iteration: {i}")
